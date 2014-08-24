812ee77481ce497d24212d5a8d8b3704755ccf59
Rob Hunter <robertjhunter@gmail.com>
2014-08-24 12:16:39 +1000

    spawn button shows count

--- game/states/play.js
+++ game/states/play.js
@@ -12,42 +12,39 @@
       this.mapLayer.debug = true;
       this.desireCloseness = true;
       this.showCloseness();
-      this.numberOfDudes = 2;
-      this.sprites = [
-        this.game.add.sprite(0, 50, 'dude1'),
-        this.game.add.sprite(100, 50, 'dude2'),
-      ];
+      this.numberOfDudes = 0;
+      this.dudesGroup = this.game.add.group();
 
       this.uiZoomButton = this.game.add.button(100,5,'zoombutton',this.onZoomButton, this);
       this.uiDudeSpawner = this.game.add.button(5,5,'dude1',this.onSpawnButton, this);
+      var counterStyle = {font: '20px Arial', fill: '#d0d0d0', stroke: '#303030', strokeThickness: 4, align: 'left'};
+      this.uiDudeCountLabel = this.game.add.text(15,60, '', counterStyle);
       this.game.physics.startSystem(Phaser.Physics.ARCADE);
-      this.game.physics.arcade.enable(this.sprites[0]);
-      this.game.physics.arcade.enable(this.sprites[1]);
-      this.sprites[0].body.velocity.x = 25;
-      this.sprites[1].body.velocity.x = -25;
-      this.sprites[0].body.tilePadding.set(3);
-      this.sprites[0].body.collideWorldBounds = true;
-      this.sprites[1].body.collideWorldBounds = true;
-      this.sprites[0].body.bounce.set(1.0);
-      this.sprites[1].body.bounce.set(1.0);
     },
     update: function() {
-      this.game.physics.arcade.collide(this.sprites[0], this.mapLayer);
-      this.game.physics.arcade.collide(this.sprites[1], this.mapLayer);
-      this.game.physics.arcade.collide(this.sprites[0], this.sprites[1]);
-
+      this.game.physics.arcade.collide(this.dudesGroup, this.mapLayer);
       this.showCloseness();
+      this.showNumbers();
     },
     render: function() {
-      this.game.debug.bodyInfo(this.sprites[0], 32, 32);
+      // this.game.debug.bodyInfo(this.sprites[0], 32, 32);
     },
     onZoomButton: function() {
       console.debug('zooooom');
       this.desireCloseness = !this.desireCloseness;
     },
     onSpawnButton: function() {
-      console.debug('spawwwwn');
       this.numberOfDudes++;
+      var sprite = this.game.add.sprite(0, 50, 'dude2');
+      this.game.physics.arcade.enable(sprite);
+      sprite.body.velocity.x = 25;
+      sprite.body.tilePadding.set(3);
+      sprite.body.collideWorldBounds = true;
+      sprite.body.bounce.set(1.0);
+      this.dudesGroup.add(sprite);
+    },
+    showNumbers: function() {
+      this.uiDudeCountLabel.setText(this.numberOfDudes.toString());
     },
     showCloseness: function() {
       var multiplier = this.desireCloseness ? 4 : 1;
