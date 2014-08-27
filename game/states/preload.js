48b08b4af1d52c7e2014b3cc99b6999025d88cf6
Rob Hunter <robertjhunter@gmail.com>
2014-08-27 11:44:53 +1000

    The preloader is meant to be in the middle

--- game/states/preload.js
+++ game/states/preload.js
@@ -15,7 +15,7 @@ window.PhaserGlobal = {disableWebAudio: /Firefox/.test(navigator.userAgent)};
 
 Preload.prototype = {
   preload: function() {
-    this.asset = this.add.sprite(this.width/2,this.height/2, 'preloader');
+    this.asset = this.add.sprite(this.game.width/2,this.game.height/2, 'preloader');
     this.asset.anchor.setTo(0.5, 0.5);
 
     this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
