4da3f521fa145f3bb3dd00b4aca5ca6c7ccf3a99
Rob Hunter <robertjhunter@gmail.com>
2014-08-28 14:45:06 +1000

    the display scales for browsers (inc android)
    - scale game in code rather than CSS

--- game/states/boot.js
+++ game/states/boot.js
@@ -6,6 +6,12 @@ function Boot() {
 
 Boot.prototype = {
   preload: function() {
+    this.game.scale.maxWidth = 1024;
+    this.game.scale.maxHeight = 768;
+    this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.game.scale.pageAlignHorizontally = true;
+    this.game.scale.setScreenSize();
+
     this.load.image('preloader', 'assets/preloader.gif');
   },
   create: function() {
