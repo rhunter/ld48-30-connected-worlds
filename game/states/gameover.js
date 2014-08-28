43777cbb1999e3be4f9c900e5a8617b2040f731b
Rob Hunter <robertjhunter@gmail.com>
2014-08-28 12:43:14 +1000

    game over screen stays centred

--- game/states/gameover.js
+++ game/states/gameover.js
@@ -10,15 +10,21 @@ GameOver.prototype = {
     var style = { font: '65px Arial', fill: '#ffffff', align: 'center'};
     this.titleText = this.game.add.text(this.game.world.centerX,100, 'Game Over!', style);
     this.titleText.anchor.setTo(0.5, 0.5);
+    this.titleText.fixedToCamera = true
 
-    this.congratsText = this.game.add.text(this.game.world.centerX, 200, 'You Win!', { font: '32px Arial', fill: '#ffffff', align: 'center'});
+    this.congratsText = this.game.add.text(this.game.world.centerX, 200, 'And that was just the beginning...', { font: '32px Arial', fill: '#ffffff', align: 'center'});
     this.congratsText.anchor.setTo(0.5, 0.5);
+    this.congratsText.fixedToCamera = true
 
     this.instructionText = this.game.add.text(this.game.world.centerX, 300, 'Click To Play Again', { font: '16px Arial', fill: '#ffffff', align: 'center'});
     this.instructionText.anchor.setTo(0.5, 0.5);
+    this.instructionText.fixedToCamera = true
 
     this.music = this.game.add.sound('winmusic');
     this.music.play();
+  },
+  init: function() {
+    this.game.world.setBounds(0,0,800,600);
   },
   update: function () {
     if(this.game.input.activePointer.justPressed()) {
